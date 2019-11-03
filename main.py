d894ecacaa3c4e9906ad81a45b2e0abfc8a6fcf9
cache-sk <cache.sk@gmail.com>
2019-11-03 01:44:33 +0100

    r.i.p.

--- main.py
+++ main.py
@@ -5,7 +5,23 @@
 # License: AGPL v.3 https://www.gnu.org/licenses/agpl-3.0.html
 
 import sys
-import topserialy
+#import topserialy
+import xbmcgui, xbmcaddon, xbmcplugin
+
+_url = sys.argv[0]
+_handle = int(sys.argv[1])
+_addon = xbmcaddon.Addon()
 
 if __name__ == '__main__':
-    topserialy.router(sys.argv[2][1:])
+    #topserialy.router(sys.argv[2][1:])
+    disc = """
+Sluzby verystream, oload, streamango a rapidvideo ukoncili svoju prevadzku, vzhladom na fakt, ze vsetky odkazy na videa boli
+z tychto zdrojov sme sa rozhodli ukoncit prevadzku aj my.
+
+Farewell, RIP.
+
+
+Doplnok si tym padom mozte odstranit, uz nikdy nebude fungovat.
+    """
+    xbmcgui.Dialog().textviewer(_addon.getAddonInfo('name'), disc)
+    xbmcplugin.endOfDirectory(_handle)
